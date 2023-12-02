2c44eb87321311aadcd1c2cfbbf28396eeb3d756
Chaojie <hi@chaojie.fun>
2023-12-02 15:33:56 +0800

    Add rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://chaojie.fun",
   integrations: [
     UnoCSS({
       injectReset: false,
